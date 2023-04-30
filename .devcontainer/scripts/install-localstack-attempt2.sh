ff88b0ed9cf7149afb77ea6bf591a06c6c902e07
Frank Peng <88225527+frankpengau@users.noreply.github.com>
2023-04-30 20:16:50 +1000

    working example 2

--- .devcontainer/scripts/install-localstack-attempt2.sh
+++ .devcontainer/scripts/install-localstack-attempt2.sh
@@ -4,6 +4,15 @@ set -euo pipefail
 function pre-checks() {
     echo "pre-requisite checks..."
 
+    current_user=`whoami`
+    echo "Using User: $current_user"
+
+    current_user_settings=`cat /etc/passwd | grep $current_user`
+    echo "Using user settings: $current_user_settings"
+
+    current_group_settings=`cat /etc/group | grep $current_user`
+    echo "Using group settings: $current_group_settings"
+
     # os_arch=`uname -a`
     os_arch=`uname -sm`
     echo "Using OS: $os_arch"
@@ -30,27 +39,41 @@ function pre-checks() {
 function install_localstack_and_awslocal() {
   echo "Installing LocalStack and awslocal..."
   python3 -m pip install localstack awscli-local
+  # python3 -m pip install --user localstack awscli-local
 }
 
 function add_alias_for_awslocal() {
   echo "Adding alias for awslocal..."
-  echo "alias aws='awslocal'" >> ~/.bashrc
+  echo "alias aws='awslocal'" >> /home/vscode/.bashrc
+  source /home/vscode/.bashrc
+
+  # echo "alias aws='awslocal'" >> ~/.bashrc
   # source ~/.bashrc
 
   # echo "alias aws='awslocal'" >> /root/.bashrc
   # source /root/.bashrc
 }
 
+function setup_localstack() {
+  echo "Setting up localstack..."
+  sudo mkdir -p /var/lib/localstack
+  # sudo chmod -R 775 /var/lib/localstack
+  sudo chmod -R 777 /var/lib/localstack
+
+  # chmod +x "./.devcontainer/scripts/start-localstack.sh"
+}
+
 function start_localstack() {
-    # chmod +x "./.devcontainer/scripts/start-localstack.sh"
     echo "Starting up localstack..."
     ./.devcontainer/scripts/start-localstack.sh
+    # echo "Localstack available on https://0.0.0.0:4566 or localhost:4566"
 }
 
 main() {
     pre-checks
     install_localstack_and_awslocal
     add_alias_for_awslocal
+    setup_localstack
     start_localstack
 }
 
